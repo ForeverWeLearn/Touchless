719e0e24c1732eb3cc6b325f22cf6e48fa8b9823
Fowel <theanh11052006@gmail.com>
2025-03-03 02:14:49 +0700

    feat: sidebar for flowchart page

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { useSvelteFlow } from "@xyflow/svelte";
+  import { save_chart } from "../scripts/utils/writer";
+
+  const { fitView, getViewport } = useSvelteFlow();
+
+  async function save_and_reload() {
+    await save_chart(getViewport());
+  }
+</script>
+
+<aside>
+  <div class="row">
+    <div class="col me-2">
+      <div class="row">
+        <button class="btn btn-sm btn-secondary mt-1" onclick={() => fitView({ duration: 1000 })}>Fit View</button>
+        <button class="btn btn-sm btn-success mt-1" onclick={() => save_and_reload()}>Save</button>
+      </div>
+    </div>
+  </div>
+</aside>
+
+<style>
+  aside {
+    position: absolute;
+    right: 10px;
+    top: 80px;
+    width: 4em;
+    background: transparent;
+    z-index: 997;
+  }
+</style>
