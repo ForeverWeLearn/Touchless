00538ad0ea04874b1937b814dbba6d5adb912730
pain_ <handstand30s@gmail.com>
2025-03-07 21:47:02 +0700

    feat: add plugin single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -49,11 +49,18 @@ pub struct KeySequenceEntry {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // .plugin(tauri_plugin_single_instance::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            let window = app.get_webview_window("main").unwrap();
+            let _ = window.unminimize();
+            let _ = window.show();
+            let _ = window.set_focus();
+        }))
         .manage(AppState {
             enigo: Mutex::new(Enigo::new(&Settings::default()).unwrap()),
         })
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             perform_hotkey,
             mouse,
