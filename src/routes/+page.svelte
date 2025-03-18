0306a4591ddd130671dd68a7c36c32869af661ad
Fowel <theanh11052006@gmail.com>
2025-03-19 06:56:46 +0700

    chore: edit home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,7 @@
-<script>
+<script lang="ts">
   import Welcome from "../components/home/Welcome.svelte";
 </script>
 
-<div class="d-flex flex-column justify-content-center align-items-center h-100" data-tauri-drag-region>
+<div class="d-flex flex-column justify-content-center align-items-center h-100 home-page-container" data-tauri-drag-region>
   <Welcome></Welcome>
 </div>
-
-<style>
-  div {
-    background-image: url("imgs/image.png");
-    background-size: cover;
-  }
-</style>
