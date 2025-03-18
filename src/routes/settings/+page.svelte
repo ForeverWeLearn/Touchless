18d9ab33a521bcb5266ba557584dd2873528bc66
Fowel <theanh11052006@gmail.com>
2025-03-19 06:57:12 +0700

    chore: edit settings page

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,9 +1,9 @@
-<script>
+<script lang="ts">
   import { theme } from "../../stores/appearance.svelte";
 </script>
 
-<h1>Settings</h1>
-<select bind:value={theme.default} style="text-transform: capitalize;">
+<h1>Settings (in progress)</h1>
+<select bind:value={theme.current} style="text-transform: capitalize;">
   {#each theme.list as th}
     <option value={th} style="text-transform: capitalize;">{th}</option>
   {/each}
