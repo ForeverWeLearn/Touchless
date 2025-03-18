8028cb17d342f7d576e25e8400abadb8f7fc5705
Fowel <theanh11052006@gmail.com>
2025-03-19 06:56:12 +0700

    feat: dynamic window border

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,46 +1,59 @@
-<script>
+<script lang="ts">
   import "bootstrap/dist/css/bootstrap.min.css";
   import "bootstrap/dist/js/bootstrap.min.js";
 
+  import { getCurrentWindow } from "@tauri-apps/api/window";
+  import { layout, theme } from "../stores/appearance.svelte";
+  import { handleKeyDown } from "../stores/user_key.svelte";
   import { engine_state } from "../stores/engine.svelte";
   import { Engine } from "../scripts/engine.svelte";
-  import { layout, theme } from "../stores/appearance.svelte";
   import Titlebar from "../components/Titlebar.svelte";
   import Sidebar from "../components/Sidebar.svelte";
   import Board from "../components/preview/Board.svelte";
+
+  const appWindow = getCurrentWindow();
 
   const engine = new Engine();
 
   let { children } = $props();
 
+  let main: HTMLElement;
+
   $effect(() => {
     engine.set_state(engine_state.running);
   });
 
+  appWindow.onResized(async ({ payload: size }) => {
+    console.log("Window resized", size);
+    if (await appWindow.isMaximized()) {
+      main.classList.remove("window-decoration");
+    } else {
+      main.classList.add("window-decoration");
+    }
+  });
+
   document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+  window.addEventListener("keydown", handleKeyDown);
 </script>
 
 <svelte:head>
   <link rel="stylesheet" href="css/main.css" />
-  <link rel="stylesheet" href="css/layouts/{layout.default}/layout.css" />
-  <link rel="stylesheet" href="css/themes/{theme.default}/theme.css" />
+  <link rel="stylesheet" href="css/layouts/{layout.current}/layout.css" />
+  <link rel="stylesheet" href="css/themes/{theme.current}/theme.css" />
 </svelte:head>
 
 <Titlebar></Titlebar>
 
-<div class="d-flex h-100">
-  <div class="d-flex sidebar-container" style="z-index: 10000;">
-    <Sidebar></Sidebar>
-  </div>
+<main class="window-decoration appear" bind:this={main}>
+  <div class="d-flex h-100">
+    <div class="d-flex sidebar-container" style="z-index: 9999;">
+      <Sidebar></Sidebar>
+    </div>
 
-  <div class="d-flex flex-column flex-fill" style="position: relative;">
-    <Board></Board>
-    {@render children()}
+    <div class="d-flex flex-column flex-fill page-container">
+      <Board></Board>
+      {@render children()}
+    </div>
   </div>
-</div>
-
-<style>
-  .sidebar-container {
-    padding: 1rem;
-  }
-</style>
+</main>
