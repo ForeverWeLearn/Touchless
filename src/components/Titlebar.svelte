04cdbb904a55b1be9d7c6554a7735f648add4e5b
Fowel <theanh11052006@gmail.com>
2025-03-24 01:40:44 +0700

    feat: titlebar button group icon customize

--- src/components/Titlebar.svelte
+++ src/components/Titlebar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { getCurrentWindow } from "@tauri-apps/api/window";
+  import { titlebarBtnGroup } from "../stores/appearance.svelte";
 
   const appWindow = getCurrentWindow();
 </script>
@@ -11,21 +12,21 @@
       class="btn d-flex justify-content-center align-items-center btn-titlebar"
       onclick={() => appWindow.minimize()}
     >
-      <img src="imgs/titlebar/circle/minimize.svg" alt="Minimize" />
+      <img src="imgs/titlebar/{titlebarBtnGroup.current}/minimize.svg" alt="Minimize" />
     </button>
     <button
       id="titlebar-maximize"
       class="btn d-flex justify-content-center align-items-center btn-titlebar"
       onclick={() => appWindow.toggleMaximize()}
     >
-      <img src="imgs/titlebar/circle/maximize.svg" alt="Maximize" />
+      <img src="imgs/titlebar/{titlebarBtnGroup.current}/maximize.svg" alt="Maximize" />
     </button>
     <button
       id="titlebar-close"
       class="btn d-flex justify-content-center align-items-center btn-titlebar"
       onclick={() => appWindow.close()}
     >
-      <img src="imgs/titlebar/circle/close.svg" alt="Close" />
+      <img src="imgs/titlebar/{titlebarBtnGroup.current}/close.svg" alt="Close" />
     </button>
   </div>
 </div>
--- src/stores/appearance.svelte.ts
+++ src/stores/appearance.svelte.ts
@@ -1,6 +1,6 @@
 export const theme = $state({
-  default: "light",
-  current: "light",
+  default: "dark",
+  current: "dark",
   list: ["dark", "light"],
 });
 
@@ -8,4 +8,10 @@ export const layout = $state({
   default: "flex",
   current: "flex",
   list: ["flex", "chaos"],
+});
+
+export const titlebarBtnGroup = $state({
+  default: "circle",
+  current: "circle",
+  list: ["circle", "round"],
 });
