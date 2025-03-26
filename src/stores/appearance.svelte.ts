54b6e1517a77fab606c0dd9acf3a3ec7678568a9
Fowel <theanh11052006@gmail.com>
2025-03-27 02:41:47 +0700

    feat: add theme colors to theme store

--- src/stores/appearance.svelte.ts
+++ src/stores/appearance.svelte.ts
@@ -1,10 +1,25 @@
-export const theme = $state({
+export const themeStore = $state({
   default: "dark",
   current: "dark",
-  list: ["dark", "light"],
+  list: [
+    {
+      name: "dark",
+      colors: [
+        ["#101010", "#181818", "#252525", "#2f2f2f", "#3c3c3c"],
+        ["#fff", "#ededed", "#dfdfdf", "#c6c6c6", "#afafaf"],
+      ],
+    },
+    {
+      name: "light",
+      colors: [
+        ["#fff", "#ededed", "#dfdfdf", "#c6c6c6", "#afafaf"],
+        ["#101010", "#181818", "#252525", "#2f2f2f", "#3c3c3c"],
+      ],
+    },
+  ],
 });
 
-export const layout = $state({
+export const layoutStore = $state({
   default: "flex",
   current: "flex",
   list: ["flex", "chaos"],
