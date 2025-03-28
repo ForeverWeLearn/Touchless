68be3087d2ff9955d3413f8cc90b99424964348a
Fowel <theanh11052006@gmail.com>
2025-03-29 01:29:30 +0700

    fix: unit display

--- src/components/flow/nodes/components/Slider.svelte
+++ src/components/flow/nodes/components/Slider.svelte
@@ -51,18 +51,17 @@
   {/if}
 
   {#if showvalue}
-    <div class="d-flex" style="position: absolute; top: 0; right: 0.15rem; bottom: 0; display:flex; align-items:center">
+    <div class="d-flex gap-1" style="position: absolute; top: 0; right: 0.65rem; bottom: 0; display:flex; align-items:center">
       <input class="form-control input-slider" type="number" {min} {max} {step} bind:value />
-      {#if unit}<small>{unit}</small>{/if}
+      {#if unit}<span>{unit}</span>{/if}
     </div>
   {/if}
 </div>
 
 <style>
-  small {
-    position: absolute;
+  span {
+    font-size: 0.8rem;
     mix-blend-mode: difference;
-    right: 0.6rem;
   }
 
   img {
@@ -114,26 +113,19 @@
   input[type="number"] {
     width: 4rem;
     padding: 0.1rem;
-    margin-right: 0.4rem;
 
-    text-align: right;
-    padding-right: 1.4rem;
+    text-align: center;
 
     mix-blend-mode: difference;
-    /* background-color: var(--bg-color-0) !important; */
     background-color: transparent !important;
 
     font-size: 0.8rem;
     color: var(--fg-color-0) !important;
-    /* border: none; */
     border-radius: calc(var(--btn-border-radius) / 2);
-    outline: none;
   }
 
   input[type="number"]:focus {
-    /* border: none; */
     border-color: var(--fg-color-0);
-    outline: none;
     box-shadow: none;
   }
 
